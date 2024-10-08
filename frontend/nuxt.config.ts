b24f18f86859f0dec083f5229ad8720574279980
kpeter1994 <smitpeter777@gmail.com>
2024-10-08 19:28:44 +0200

    Initial commit

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/strapi'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    admin: '/admin',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt'
+  }
+})
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+const { find } = useStrapi()
+
+const response = await find('articles')
+
+</script>
+
+<template>
+  <div>
+    <pre>
+      {{response}}
+    </pre>
+  </div>
+</template>
+
+<style scoped></style>
